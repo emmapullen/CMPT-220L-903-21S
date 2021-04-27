ab4a3a8be4cb8c4cf10234ac6b990ba9c23ef45a
emmapullen <79120671+emmapullen@users.noreply.github.com>
2021-04-26 21:12:20 -0400

    Lab 6& others

---
+++ Assignment 6/src/base/Main.java
@@ -0,0 +1,11 @@
+package base;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Plant rose=new Plant("flower", "rose");
+        rose.getInfo();
+        rose.speak();
+	// write your code here
+    }
+}
